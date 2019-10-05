18a1c54d5fcffec4551b43685f2d91b258f5c28b
CJ Gladback <33671915+cjgladback@users.noreply.github.com>
2019-10-05 15:49:44 -0400

    Starting to put things together
    Working on followng Unity + Ink tutorials from Dan Cox to get some crossover between the story and eventual visuals.

---
+++ LD45 Gamification/Assets/Scripts/InkTesting.cs
@@ -0,0 +1,23 @@
+﻿using Ink.Runtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InkTesting : MonoBehaviour
+{
+    public TextAsset inkJSON;
+    private Story story;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        story = new Story(inkJSON.text);
+        Debug.Log(story.ContinueMaximally());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
